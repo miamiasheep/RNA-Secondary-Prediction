3b24626eb3ccfa4256bcefcb5763712a97ffe856
ericlee <lee3388@purdue.edu>
2020-11-17 22:50:15 -0500

    :sparkles: Implement scoring function

--- nussinov/main.py
+++ nussinov/main.py
@@ -3,10 +3,20 @@ import numpy as np
 def initialize(n):
     return np.zeros((n, n))
 
-def pair_check(tup):
-    if tup in [('A', 'U'), ('U', 'A'), ('C', 'G'), ('G', 'C')]:
-        return True
-    return False
+
+scores = {
+            ('A', 'U'): 1,
+            ('C', 'G'): 1
+}
+
+
+def get_scores(tup):
+    if tup in scores:
+        return scores[tup]
+    if tup[::-1] in scores:
+        return scores[tup[::-1]]
+    return 0
+
 
 if __name__ == '__main__':
     seq = 'CGAGUCGGAGUC'
@@ -14,7 +24,6 @@ if __name__ == '__main__':
     dp = initialize(n)
     print(dp)
     # fill in the dp array
-    print('===============\n')
     for k in range(n):
         for i in range(n-k):
             j = i + k
@@ -23,10 +32,8 @@ if __name__ == '__main__':
             else:
                 opts = [dp[i][j-1]]
                 for t in range(i, j):
-                    if pair_check((seq[t], seq[j])):
-                        opts.append(1 + dp[i][t-1] + dp[t+1][j-1])
+                    opts.append(get_scores((seq[t], seq[j])) + dp[i][t-1] + dp[t+1][j-1])
                 opts.append(0)
-                print(opts)
                 dp[i][j] = max(opts)
             # find the optimal solution
     print(dp)
