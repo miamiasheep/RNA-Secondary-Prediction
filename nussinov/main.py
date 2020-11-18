ec24508ea85e3749941593a806f17675f6e1483e
ericlee <lee3388@purdue.edu>
2020-11-17 22:24:50 -0500

    :sparkles: Implement dynamic programming

--- nussinov/main.py
+++ nussinov/main.py
@@ -1,14 +1,32 @@
 import numpy as np
 
 def initialize(n):
-    dp = np.empty((n, n))
-    dp[:] = np.NAN
-    # fill the dp array with zero
-    for i in range(n):
-        j = i
-        dp[i][j] = 0
-    return dp
+    return np.zeros((n, n))
+
+def pair_check(tup):
+    if tup in [('A', 'U'), ('U', 'A'), ('C', 'G'), ('G', 'C')]:
+        return True
+    return False
 
 if __name__ == '__main__':
-    dp = initialize(6)
+    seq = 'CGAGUCGGAGUC'
+    n = len(seq)
+    dp = initialize(n)
+    print(dp)
+    # fill in the dp array
+    print('===============\n')
+    for k in range(n):
+        for i in range(n-k):
+            j = i + k
+            if i >= j:
+                dp[i][j] = 0
+            else:
+                opts = [dp[i][j-1]]
+                for t in range(i, j):
+                    if pair_check((seq[t], seq[j])):
+                        opts.append(1 + dp[i][t-1] + dp[t+1][j-1])
+                opts.append(0)
+                print(opts)
+                dp[i][j] = max(opts)
+            # find the optimal solution
     print(dp)
